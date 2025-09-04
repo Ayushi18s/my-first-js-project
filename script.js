1a9c7fa4b9454ef1b47a5777aeaccdd34fb7b2bc
Ayushi <18ayushi.s@gmail.com>
2025-09-04 22:48:24 +0530

    Day 1 - Completed

---
+++ script.js
@@ -0,0 +1,16 @@
+//Day 1 : Your First Line of code and variables
+
+//Lesson 1 : Using console.log() to print a message 
+console.log("Hello, world!");
+console.log("My forst time learning javascript");
+
+//Lesson 2: Storing data in variables 
+let myName = "Ayushi";
+let currentYear = 2025;
+
+console.log(myName);
+console.log(currentYear);
+
+//Changing the value of a variable later
+currentYear = 2025;
+console.log(currentYear);
